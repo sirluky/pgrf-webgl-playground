98a062bf79539ab3a3569f5b32cc016ff4e1520d
sirluky <38957065+sirluky@users.noreply.github.com>
2023-03-20 23:46:34 +0100

    1 file modified, 1 deleted

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 const vertexShaderSource = `#version 300 es
 
+uniform float uPSize;
+uniform vec2 uPos;
+
 void main() {
-  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-  gl_PointSize = 100.0;
+  gl_Position = vec4(uPos,0.0,1.0);
+  gl_PointSize = uPSize;
 }
 `;
 const fragmentShaderSource = `#version 300 es
 
 precision mediump float;
 
+uniform int uColIndex;
+uniform vec4 uColors[4];
+
 out vec4 fragColor;
 
 void main() {
-  fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  fragColor = uColors[uColIndex]; 
 }`;
 
 const canvas = document.querySelector('canvas');
@@ -41,4 +47,29 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 gl.useProgram(program);
 
+const uPSize = gl.getUniformLocation(program, 'uPSize');
+const uPos = gl.getUniformLocation(program, 'uPos');
+// const uCol = gl.getUniformLocation(program, 'uCol');
+const uColIndex = gl.getUniformLocation(program, 'uColIndex');
+const uColors = gl.getUniformLocation(program, 'uColors');
+// console.log(uCol, uPSize);
+
+gl.uniform1f(uPSize, 100);
+gl.uniform2f(uPos, 0.1, 0.5);
+
 gl.drawArrays(gl.POINTS, 0, 1);
+
+for (let i = 0; i < 100; i++) {
+  gl.uniform1f(uPSize, Math.random() * 30);
+  gl.uniform2f(uPos, Math.random() * 2 - 1, Math.random() * 2 - 1);
+  gl.uniform1i(uColIndex, Math.floor(Math.random() * 4));
+
+  gl.uniform4fv(uColors, [
+      1, 0, 0, 1
+    , 1, 1, 0, 1 
+    , 1, 1, 1, 1
+    , 0, 1, 0, 1
+  ]);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
